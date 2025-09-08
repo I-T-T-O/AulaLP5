3f9e3ffd9cfb3dbff392f7bc70f482513517581a
I-T-T-O <ian.oshiro@estudante.ifms.edu.br>
2025-09-08 11:40:24 -0400

    Finalizei parcialmente o Util.java

--- src/tools/Util.java
+++ src/tools/Util.java
@@ -4,7 +4,10 @@
  */
 package tools;
 
+import java.util.Date;
+import javax.swing.JComboBox;
 import javax.swing.JComponent;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 /**
@@ -15,14 +18,42 @@ public class Util {
 
     public static void habilitar(boolean valor, JComponent... componentes) {
         for (int i = 0; i < componentes.length; i++) {
-            componentes[i].setEnabled(valor);            
+            componentes[i].setEnabled(valor);
         }
     }
-    
-    public static void limpar(JTextField ... componentes) {
+
+    public static void limpar(JComponent... componentes) {
         for (int i = 0; i < 10; i++) {
-           ((JTextField) componentes[i]).setText("");
+            if (componentes[i] instanceof JTextField) {
+                ((JTextField) componentes[i]).setText("");
+            }
+            if (componentes[i] instanceof JComboBox) {
+                ((JComboBox) componentes[i]).setSelectedIndex(-1);
+            }
         }
+    }
+    public static void mensagem(String cad){
+        JOptionPane.showMessageDialog(null, cad);
         
+    }
+    public static boolean perguntar(String cad){
+        JOptionPane.showConfirmDialog(null, cad);
+        return true;
+        
+    }
+    public static int strToInt(String cad){
+        return Integer.valueOf(cad);
+    }
+    public static String intToStr(int num){
+        return String.valueOf(num);
+    }
+    public static double strToDouble(String cad){
+        return Double.valueOf(cad);
+    }
+    public static String doubleToStr(double num){
+        return String.valueOf(num);
+    }
+    public static String strToDate(Date data){
+        return String.valueOf(data);
     }
 }
--- src/view/JDlgUsuarios.java
+++ src/view/JDlgUsuarios.java
@@ -263,7 +263,7 @@ public class JDlgUsuarios extends javax.swing.JDialog {
 
     private void jBtnExcluirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtnExcluirActionPerformed
         // TODO add your handling code here:
-
+        Util.perguntar("deseja exlcuir essa porra?");
     }//GEN-LAST:event_jBtnExcluirActionPerformed
 
 
@@ -281,6 +281,7 @@ public class JDlgUsuarios extends javax.swing.JDialog {
     private void jBtnPesquisarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtnPesquisarActionPerformed
         // TODO add your handling code here: 
 
+        Util.mensagem("não implementado");
 
     }//GEN-LAST:event_jBtnPesquisarActionPerformed
 
